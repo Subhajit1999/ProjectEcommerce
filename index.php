24b5d6b07aeaf64616792b2a96a31271a81eb1c8
Subhajit1999 <developersubha123@gmail.com>
2021-03-11 13:54:41 +0530

    completed home (almost)

--- index.php
+++ index.php
@@ -22,8 +22,7 @@
 
     <!-------- Header (Toolbar & Navigation Bar)------------>
     <?php
-        include 'common/header.html';
-    ?>
+        include 'common/header.html'; ?>
 
     <!-- Banner Carousel -->
     <div class="banner">
@@ -36,16 +35,16 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="img/img-2.jpeg" class="d-block w-100">
+                    <img src="img/img1.jpg" class="d-block w-100">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/img-3.jpeg" class="d-block w-100">
+                    <img src="img/img2.jpg" class="d-block w-100">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/img-1.jpeg" class="d-block w-100">
+                    <img src="img/img3.jpg" class="d-block w-100">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/img-4.jpeg" class="d-block w-100">
+                    <img src="img/img4.jpg" class="d-block w-100">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#banner-carousel" role="button" data-bs-slide="prev">
@@ -59,9 +58,44 @@
         </div>
     </div>
 
+    <!-- Products Section -->
     <?php
-        include 'common/footer.html';
-    ?>
+        include 'components/products-main.html'; ?>
+
+    <!-- website features -->
+    <section class="website-features">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 feature-box">
+                    <img src="img/feature-1.png">
+                    <div class="feature-text">
+                        <p><b>100% Original items </b>are available at company</p>
+                    </div>
+                </div>
+                <div class="col-md-3 feature-box">
+                    <img src="img/feature-2.png">
+                    <div class="feature-text">
+                        <p><b>Return within 30 days </b>of receiving your order.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 feature-box">
+                    <img src="img/feature-3.png">
+                    <div class="feature-text">
+                        <p><b>Get free delivery for every </b>order on more than price.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 feature-box">
+                    <img src="img/feature-4.png">
+                    <div class="feature-text">
+                        <p><b>Pay Online through multiple option</b>(card/Net banking)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php
+        include 'common/footer.html'; ?>
 </body>
 
 </html>
